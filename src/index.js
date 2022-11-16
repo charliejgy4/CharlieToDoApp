59c019fe4a2f4988544ac2cc0dcf025394cc65dc
Charlie Joseph <charliejgy4@gmail.com>
2022-11-16 17:00:10 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import index from'./index'
+// import FilterButton from './FilterButton'
+
+
+const DATA = [
+  { id: "todo-0", name: "Meeting with Doctor", completed: true },
+  { id: "todo-1", name: "Meeting with Business Partners", completed: false },
+  { id: "todo-2", name: "Projects to complete", completed: false }
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+export default index
